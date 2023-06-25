6f9537b11ad740e55f165b3a323527d45f3baaef
angone <anthony.angone322@gmail.com>
2023-06-25 17:39:10 -0400

    .

---
+++ downloadGraph.py
@@ -0,0 +1,11 @@
+import boto
+import sys
+
+conn = boto.connect_s3(anon=True)
+b = conn.get_bucket("mqlibinstances",validate=False)
+k = boto.s3.key.Key(b)
+k.key = sys.argv[1]
+if sys.version_info > (3, 0):
+	sys.stdout.buffer.write(k.get_contents_as_string())
+else:
+	sys.stdout.write(k.get_contents_as_string())
