1545b59e50a4738fde6cd362128c49194f1815e5
angone <anthony.angone322@gmail.com>
2023-06-28 18:58:56 -0400

    .

--- maxcut2.py
+++ maxcut2.py
@@ -467,7 +467,7 @@ class MaxcutSolver:
             for i in range(len(S)):
                 S[i] = self.solution[fineToCoarse[i]]
             self.solution = S
-            if True:
+            if False:
                 R = Refinement(E.G, self.spsize, 'mqlib', self.solution)
                 R.refineLevel()
                 #R.test()
