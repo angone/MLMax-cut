2aececd1001a8d0a177355c094d254185050a16f
angone <anthony.angone322@gmail.com>
2023-06-29 01:04:03 -0400

    .

--- maxcut2.py
+++ maxcut2.py
@@ -65,7 +65,7 @@ class EmbeddingCoarsening:
         n = self.G.numberOfNodes()
         embeddings = []
         b = self.buildObj()
-        bnds = [[(-1,1) for _ in range(self.d)] for _ in range(n)]
+        bnds = [(-1,1)] * (n*self.d)
         p = [[self.space[i][j] for j in range(self.d)] for i in range(n)]
         def sphere(x):
             return np.sqrt(x[0]**2 + x[1]**2 + x[2]**2) - 1
