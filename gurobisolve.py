540793e27ecc2ed92c930eb7d2dc2a5562e74768
angone <anthony.angone322@gmail.com>
2023-07-11 04:06:54 -0400

    .

--- gurobisolve.py
+++ gurobisolve.py
@@ -1,8 +1,16 @@
-import networkit as nk
+import networkit as nw
 from gurobipy import Model, GRB
-graph = nk.graph.Graph()
+import sys
+
+def calc_obj(G, solution):
+    obj = 0
+    for u, v in G.iterEdges():
+         obj += G.weight(u, v)*(2*solution[u]*solution[v] - solution[u] - solution[v])
+    return -1 * obj
+
+graph = nw.readGraph("./graphs/"+sys.argv[1], nw.Format.EdgeListSpaceOne)
 model = Model("Max-Cut")
-model.setParam(GRB.Param.TimeLimit, 60)
+model.setParam(GRB.Param.TimeLimit, sys.argv[2])
 model.setParam('OutputFlag', False) 
 variables = {}
 for node in graph.iterNodes():
@@ -10,6 +18,7 @@ for node in graph.iterNodes():
 
 objective = 0
 for u,v,w in graph.iterEdgesWeights():
-    objective += (2*w*variables[v]*variables[u]) - w*(variables[v] + variables[u])
+    objective += w*((2*variables[v]*variables[u]) - (variables[v] + variables[u]))
 model.setObjective(objective, GRB.MAXIMIZE)
 model.optimize()
+solution = [variables[node].x for node in graph.iterNodes()]
