3897ddb1ffc9b199eba6147a71dd57b2e7b3215f
angone <anthony.angone322@gmail.com>
2023-07-11 04:03:31 -0400

    .

--- gurobisolve.py
+++ gurobisolve.py
@@ -1,40 +1,15 @@
-import networkit as nw
-import pyomo.environ as pyo
-import sys
+import networkit as nk
+from gurobipy import Model, GRB
+graph = nk.graph.Graph()
+model = Model("Max-Cut")
+model.setParam(GRB.Param.TimeLimit, 60)
+model.setParam('OutputFlag', False) 
+variables = {}
+for node in graph.iterNodes():
+    variables[node] = model.addVar(vtype=GRB.BINARY, name=f"x_{node}")
 
-def pyomo(G, time):
-
-    opt = pyo.SolverFactory('gurobi')
-    opt.options['TimeLimit'] = time
-
-    model = pyo.ConcreteModel()
-    model.n = pyo.Param(default=G.numberOfNodes())
-    model.x = pyo.Var(pyo.RangeSet(0,model.n-1), within=pyo.Binary)
-    model.obj = pyo.Objective(expr = 0)
-    model.c = pyo.Constraint(rule=model.x[2]<=1)
-    
-    for u,v in G.iterEdges():
-        w = G.weight(u,v)
-        model.obj.expr += (2 * w * model.x[u] * model.x[v])
-        model.obj.expr += (-w * model.x[u]) + (-w * model.x[v])
-    print('solving')
-    results = opt.solve(model)
-    print('solved')
-    print(results)
-    solution = {}
-    for i in range(G.numberOfNodes()):
-        solution[i] = model.x[i].value
-        if solution[i] == None:
-            solution[i] = 0
-        if solution[i] < 0.5:
-            solution[i] = 0
-        else:
-            solution[i] = 1
-    return solution
-
-
-
-G = nw.readGraph("./graphs/"+sys.argv[1], nw.Format.EdgeListSpaceOne)
-time = int(sys.argv[2])
-print(G,time)
-pyomo(G, time)
+objective = 0
+for u,v,w in graph.iterEdgesWeights():
+    objective += (2*w*variables[v]*variables[u]) - w*(variables[v] + variables[u])
+model.setObjective(objective, GRB.MAXIMIZE)
+model.optimize()
