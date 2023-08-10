194e7e027db08f265cdec1c4604a0c07d6dbf954
angone <anthony.angone322@gmail.com>
2023-08-10 15:24:12 -0400

    .

--- maxcut.py
+++ maxcut.py
@@ -566,7 +566,7 @@ class MaxcutSolver:
                 self.obj = max_obj
                 print('Objective:',self.obj)
                 starts = max(2, int(starts/2))
-
+        print('starting mqlib')
         mqobj = R.calc_obj(self.problem_graph, R.mqlibSolve(t=sptime,G=self.problem_graph))
         print('mqlib ratio:',self.obj / mqobj)
         print('coarse ratio:', self.coarse_obj/self.obj)
